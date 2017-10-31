435b25e22fd45dda4aaafcf172862a0e5baaeea9
Matthieu PETIOT <matthieu.petiot@gmail.com>
2017-10-31 16:41:54 +0100

    Ajout de traductions oubliées.

--- resources/lang/fr_FR.ts
+++ resources/lang/fr_FR.ts
@@ -4,37 +4,37 @@
 <context>
     <name>DownloadManager</name>
     <message>
-        <location filename="../src/network/downloadmanager.cpp" line="262"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="262"/>
         <source>o/s</source>
         <extracomment>This string refers to bytes per second.</extracomment>
         <translation>ko/s</translation>
     </message>
     <message>
-        <location filename="../src/network/downloadmanager.cpp" line="267"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="267"/>
         <source>ko/s</source>
         <extracomment>This string refers to kilobytes per second.</extracomment>
         <translation>ko/s</translation>
     </message>
     <message>
-        <location filename="../src/network/downloadmanager.cpp" line="272"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="272"/>
         <source>Mo/s</source>
         <extracomment>This string refers to megabytes per second.</extracomment>
         <translation>Mo/s</translation>
     </message>
     <message>
-        <location filename="../src/network/downloadmanager.cpp" line="276"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="276"/>
         <source>%1 %2</source>
         <extracomment>This string refers to download speed as &quot;12 ko/s&quot;.</extracomment>
         <translation>%1 %2</translation>
     </message>
     <message>
-        <location filename="../src/network/downloadmanager.cpp" line="289"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="287"/>
         <source>%1 %2 %3</source>
         <extracomment>This string refers a time (hours, minutes, seconds).</extracomment>
         <translation>%1 %2 %3</translation>
     </message>
     <message numerus="yes">
-        <location filename="../src/network/downloadmanager.cpp" line="290"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="288"/>
         <source>%n heure(s)</source>
         <comment>les heures estimées</comment>
         <translation>
@@ -43,7 +43,7 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location filename="../src/network/downloadmanager.cpp" line="291"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="289"/>
         <source>%n minute(s)</source>
         <comment>les minutes estimées</comment>
         <translation>
@@ -52,7 +52,7 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location filename="../src/network/downloadmanager.cpp" line="292"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="290"/>
         <source>%n seconde(s)</source>
         <comment>les secondes estimées</comment>
         <translation>
@@ -60,57 +60,59 @@
             <numerusform>%n secondes</numerusform>
         </translation>
     </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
-        <location filename="../ui/mainwindow.ui" line="14"/>
+        <location filename="../../ui/mainwindow.ui" line="14"/>
         <source>MainWindow</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="97"/>
+        <location filename="../../ui/mainwindow.ui" line="97"/>
         <source>Installeur</source>
         <translation>Installeur</translation>
     </message>
     <message>
-        <location filename="../ui/mainwindow.ui" line="334"/>
+        <location filename="../../ui/mainwindow.ui" line="334"/>
         <source>PushButton</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/gui/mainwindow.cpp" line="69"/>
+        <location filename="../../src/gui/mainwindow.cpp" line="43"/>
         <source>Bienvenue dans l&apos;installeur de %1 !</source>
         <extracomment>This string refers to the welcome screen.</extracomment>
         <translation>Bienvenue dans l&apos;installeur de %1 !</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="48"/>
+        <location filename="../../src/gui/mainwindow.cpp" line="68"/>
         <source>Attention !</source>
         <extracomment>This string refers to the exit message title.</extracomment>
         <translation>Attention !</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="50"/>
+        <location filename="../../src/gui/mainwindow.cpp" line="70"/>
         <source>Vous êtes sur le point de quitter l&apos;application, voulez vous continuer ?</source>
         <extracomment>This string refers to the exit message.</extracomment>
         <translation>Vous êtes sur le point de quitter l&apos;application, voulez vous continuer ?</translation>
     </message>
     <message>
-        <location filename="../src/gui/mainwindow.cpp" line="151"/>
+        <location filename="../../src/gui/mainwindow.cpp" line="152"/>
         <source>Temps estimé : %1</source>
         <extracomment>This string refers to a remaining time.</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Temps estimé : %1</translation>
     </message>
     <message>
-        <location filename="../src/gui/mainwindow.cpp" line="157"/>
+        <location filename="../../src/gui/mainwindow.cpp" line="158"/>
         <source>Serveur %1</source>
         <extracomment>This string refers to a web URL.</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Serveur : %1</translation>
     </message>
     <message>
-        <location filename="../src/gui/mainwindow.cpp" line="162"/>
+        <location filename="../../src/gui/mainwindow.cpp" line="163"/>
         <source>Téléchargement de %1</source>
         <extracomment>This string refers to a downloaded file.</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Téléchargement de %1</translation>
     </message>
 </context>
 </TS>
