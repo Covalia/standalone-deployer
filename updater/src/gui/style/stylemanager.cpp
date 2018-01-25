76dd0933764188a58581f7904e0a0125ed2ab8e9
PICARD Alexis <picard.alexis@yahoo.fr>
2018-01-25 13:51:27 +0100

    Style manager dans updater avec settings et style.css dossier install

---
+++ updater/src/gui/style/stylemanager.cpp
@@ -0,0 +1,55 @@
+#include "gui/style/stylemanager.h"
+#include "settings/settings.h"
+#include "fs/apptreemanager.h"
+
+#include <QApplication>
+#include <QTextStream>
+#include <QTextCursor>
+#include <QFile>
+#include <QSettings>
+
+StyleManager::StyleManager()
+{
+}
+
+QString StyleManager::transformStyle(QString styleSheet)
+{
+    Settings * settings = Settings::getInstance();
+
+    styleSheet.replace("@color-panel-background-border", settings->getColorPanelBackgroundBorder());
+    styleSheet.replace("@color-panel-background", settings->getColorPanelBackground());
+    styleSheet.replace("@color-button-background-over", settings->getColorButtonBackgroundOver());
+    styleSheet.replace("@color-button-background", settings->getColorButtonBackground());
+    styleSheet.replace("@color-text-on-background", settings->getColorTextOnBackground());
+    styleSheet.replace("@color-text-gray", settings->getColorTextGray());
+    styleSheet.replace("@color-disabled", settings->getColorDisabled());
+    styleSheet.replace("@border-window", settings->getBorderWindow());
+
+    return styleSheet;
+}
+
+void StyleManager::transformStyle(QWidget * parentWidget)
+{
+    for (auto * widget : parentWidget->findChildren<QWidget *>()) {
+        widget->setStyleSheet(StyleManager::transformStyle(widget->styleSheet()));
+    }
+}
+
+void StyleManager::setGeneralStyle()
+{
+    Settings * settings = Settings::getInstance();
+    AppTreeManager * treeManager = new AppTreeManager(QDir(settings->getInstallLocation()));
+
+    QFile f(treeManager->getConfigurationDirPath().absolutePath() + "/style.css");
+
+    if (f.exists()) {
+        if (f.open(QFile::ReadOnly | QFile::Text)) {
+            QTextStream in(&f);
+            QString sytle = in.readAll();
+            if (!sytle.isNull() && !sytle.isEmpty()) {
+                QString transformeSytle = transformStyle(sytle);
+                qApp->setStyleSheet(transformeSytle);
+            }
+        }
+    }
+}
