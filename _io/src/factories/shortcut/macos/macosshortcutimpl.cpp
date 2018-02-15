02ce910bf0884bae60688b3bd3e12576b8a32ac7
Matthieu PETIOT <matthieu.petiot@gmail.com>
2018-02-15 16:21:00 +0100

    Return true sur l'implémentation vide des raccourcis macos.
    Évite l'affichage d'une erreur lors de la fin de l'installation.

--- _io/src/factories/shortcut/macos/macosshortcutimpl.cpp
+++ _io/src/factories/shortcut/macos/macosshortcutimpl.cpp
@@ -11,17 +11,17 @@ MacosShortcutImpl::~MacosShortcutImpl()
 bool MacosShortcutImpl::createDesktopShortcut(AppPath _appPath, QString _shortcutName, QString _args, QString _installLocation, QString _applicationName)
 {
     // TODO
-    return false;
+    return true;
 }
 
 bool MacosShortcutImpl::createStartShorcut(AppPath _appPath, QString _shortcutName, bool _allUser, QString _installLocation, QString _applicationName)
 {
     // TODO
-    return false;
+    return true;
 }
 
 bool MacosShortcutImpl::createStartMenuShorcut(AppPath _appPath, QString _startMenuFolderName, bool _allUser, QString _installLocation, QString _applicationName)
 {
     // TODO
-    return false;
+    return true;
 }
