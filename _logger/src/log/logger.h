20d139777fb152f5802ab6c46925cd349c6615c7
Matthieu PETIOT <matthieu.petiot@gmail.com>
2018-02-21 11:08:17 +0100

    Augmentation taille fichier de log avant rotation.

--- _logger/src/log/logger.h
+++ _logger/src/log/logger.h
@@ -4,7 +4,7 @@
 #include "log/simpleqtlogger.h"
 
 const unsigned int LOG_FILES_MAX = 20; // max rolling files
-const unsigned int LOG_FILE_SIZE = 200000; // size log file in byte
+const unsigned int LOG_FILE_SIZE = 2000000; // size log file in byte
 
 class Logger
 {
