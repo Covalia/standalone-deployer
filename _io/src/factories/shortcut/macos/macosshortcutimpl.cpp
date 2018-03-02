1cb89d1f29c0bcaf0a02e0650d42fb452f86318c
Matthieu PETIOT <matthieu.petiot@gmail.com>
2018-03-02 10:35:05 +0100

    Suppression de warnings dans l'implémentation vide des raccourcis macos.

--- _io/src/factories/shortcut/macos/macosshortcutimpl.cpp
+++ _io/src/factories/shortcut/macos/macosshortcutimpl.cpp
@@ -10,18 +10,30 @@ MacosShortcutImpl::~MacosShortcutImpl()
 
 bool MacosShortcutImpl::createDesktopShortcut(AppPath _appPath, QString _shortcutName, QString _args, QString _applicationName)
 {
-    // TODO
+    Q_UNUSED(_appPath)
+    Q_UNUSED(_shortcutName)
+    Q_UNUSED(_args)
+    Q_UNUSED(_applicationName)
+    // TODO implement
     return true;
 }
 
 bool MacosShortcutImpl::createStartShorcut(AppPath _appPath, QString _shortcutName, bool _allUser, QString _applicationName)
 {
-    // TODO
+    Q_UNUSED(_appPath)
+    Q_UNUSED(_shortcutName)
+    Q_UNUSED(_allUser)
+    Q_UNUSED(_applicationName)
+    // TODO implement
     return true;
 }
 
 bool MacosShortcutImpl::createStartMenuShorcut(AppPath _appPath, QString _startMenuFolderName, bool _allUser, QString _applicationName)
 {
-    // TODO
+    Q_UNUSED(_appPath)
+    Q_UNUSED(_startMenuFolderName)
+    Q_UNUSED(_allUser)
+    Q_UNUSED(_applicationName)
+    // TODO implement
     return true;
 }
