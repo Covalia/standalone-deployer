49150f38d7924487f2e8a99f06d5da0a752fc2c0
Matthieu PETIOT <matthieu.petiot@gmail.com>
2019-04-23 17:33:22 +0200

    Mise à jour des traductions

--- updater/resources/lang/en_US.ts
+++ updater/resources/lang/en_US.ts
@@ -4,50 +4,50 @@
 <context>
     <name>AskPopupUI</name>
     <message>
-        <location filename="../../ui/askpopup.ui" line="74"/>
+        <location filename="../../ui/askpopup.ui" line="77"/>
         <source>Question</source>
         <translation>Question</translation>
     </message>
     <message>
-        <location filename="../../ui/askpopup.ui" line="108"/>
+        <location filename="../../ui/askpopup.ui" line="111"/>
         <source>Do you want to exit the application?</source>
         <translation>Do you want to exit the application?</translation>
     </message>
     <message>
-        <location filename="../../ui/askpopup.ui" line="124"/>
+        <location filename="../../ui/askpopup.ui" line="127"/>
         <source>The update will be stopped and the application will not be started</source>
         <translation>The update will be stopped and the application will not be started</translation>
     </message>
     <message>
-        <location filename="../../ui/askpopup.ui" line="184"/>
+        <location filename="../../ui/askpopup.ui" line="187"/>
         <source>Yes</source>
         <translation>Yes</translation>
     </message>
     <message>
-        <location filename="../../ui/askpopup.ui" line="219"/>
+        <location filename="../../ui/askpopup.ui" line="222"/>
         <source>No</source>
         <translation>No</translation>
     </message>
 </context>
 <context>
-    <name>Dialog</name>
+    <name>AuthenticationDialogUI</name>
     <message>
-        <location filename="../../ui/authenticationdialog.ui" line="20"/>
+        <location filename="../../ui/authenticationdialog.ui" line="38"/>
         <source>Credentials are required by the remote host</source>
         <translation>Credentials are required by the remote host</translation>
     </message>
     <message>
-        <location filename="../../ui/authenticationdialog.ui" line="30"/>
+        <location filename="../../ui/authenticationdialog.ui" line="51"/>
         <source>Login</source>
         <translation>Login</translation>
     </message>
     <message>
-        <location filename="../../ui/authenticationdialog.ui" line="40"/>
+        <location filename="../../ui/authenticationdialog.ui" line="61"/>
         <source>Password</source>
         <translation>Password</translation>
     </message>
     <message>
-        <location filename="../../ui/authenticationdialog.ui" line="64"/>
+        <location filename="../../ui/authenticationdialog.ui" line="102"/>
         <source>Site</source>
         <translation>Site</translation>
     </message>
@@ -55,53 +55,37 @@
 <context>
     <name>DownloadManager</name>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="424"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="367"/>
         <source>%1 %2</source>
         <extracomment>This string refers to download speed as &quot;12 ko/s&quot;.</extracomment>
         <translation>%1 %2</translation>
     </message>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="141"/>
-        <source>HTTP authentification required</source>
-        <translation>HTTP authentication required</translation>
-    </message>
-    <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="143"/>
-        <source>%1 on %2</source>
-        <comment>The realm on host</comment>
-        <translation>%1 on %2</translation>
-    </message>
-    <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="181"/>
-        <source>Proxy authentication required</source>
-        <translation>Proxy authentication required</translation>
-    </message>
-    <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="183"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="120"/>
         <source>%1</source>
         <comment>The proxy domain</comment>
         <translation>%1</translation>
     </message>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="412"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="355"/>
         <source>B/s</source>
         <extracomment>This string refers to bytes per second.</extracomment>
         <translation>B/s</translation>
     </message>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="416"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="359"/>
         <source>kB/s</source>
         <extracomment>This string refers to kilobytes per second.</extracomment>
         <translation>kB/s</translation>
     </message>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="420"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="363"/>
         <source>MB/s</source>
         <extracomment>This string refers to megabytes per second.</extracomment>
         <translation>MB/s</translation>
     </message>
     <message numerus="yes">
-        <location filename="../../src/network/downloadmanager.cpp" line="435"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="378"/>
         <source>%n hour(s)</source>
         <comment>the estimated heures</comment>
         <translation>
@@ -110,7 +94,7 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location filename="../../src/network/downloadmanager.cpp" line="436"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="379"/>
         <source>%n minute(s)</source>
         <comment>the estimated minutes</comment>
         <translation>
@@ -119,7 +103,7 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location filename="../../src/network/downloadmanager.cpp" line="437"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="380"/>
         <source>%n second(s)</source>
         <comment>the estimated seconds</comment>
         <translation>
@@ -128,43 +112,43 @@
         </translation>
     </message>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="445"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="388"/>
         <source>%1</source>
         <comment>seconds only</comment>
         <translation>%1</translation>
     </message>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="449"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="392"/>
         <source>%1</source>
         <comment>minutes only</comment>
         <translation>%1</translation>
     </message>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="451"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="394"/>
         <source>%1 %2</source>
         <comment>minutes and seconds only</comment>
         <translation>%1 %2</translation>
     </message>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="457"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="400"/>
         <source>%1</source>
         <comment>hours only</comment>
         <translation>%1</translation>
     </message>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="459"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="402"/>
         <source>%1 %2</source>
         <comment>hours and seconds only</comment>
         <translation>%1 %2</translation>
     </message>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="463"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="406"/>
         <source>%1 %2</source>
         <comment>hours and minutes only</comment>
         <translation>%1 %2</translation>
     </message>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="465"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="408"/>
         <source>%1 %2 %3</source>
         <comment>hours minutes seconds</comment>
         <translation>%1 %2 %3</translation>
@@ -173,19 +157,19 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../../src/gui/mainwindow.cpp" line="158"/>
+        <location filename="../../src/gui/mainwindow.cpp" line="156"/>
         <source>Estimated time: %1</source>
         <extracomment>This string refers to a remaining time.</extracomment>
         <translation>Estimated time: %1</translation>
     </message>
     <message>
-        <location filename="../../src/gui/mainwindow.cpp" line="165"/>
+        <location filename="../../src/gui/mainwindow.cpp" line="163"/>
         <source>Server %1</source>
         <extracomment>This string refers to a web URL.</extracomment>
         <translation>Server %1</translation>
     </message>
     <message>
-        <location filename="../../src/gui/mainwindow.cpp" line="171"/>
+        <location filename="../../src/gui/mainwindow.cpp" line="169"/>
         <source>Downloading %1</source>
         <extracomment>This string refers to a downloaded file.</extracomment>
         <translation>Downloading %1</translation>
--- updater/resources/lang/fr_FR.ts
+++ updater/resources/lang/fr_FR.ts
@@ -4,50 +4,50 @@
 <context>
     <name>AskPopupUI</name>
     <message>
-        <location filename="../../ui/askpopup.ui" line="74"/>
+        <location filename="../../ui/askpopup.ui" line="77"/>
         <source>Question</source>
         <translation>Question</translation>
     </message>
     <message>
-        <location filename="../../ui/askpopup.ui" line="108"/>
+        <location filename="../../ui/askpopup.ui" line="111"/>
         <source>Do you want to exit the application?</source>
         <translation>Souhaitez-vous quitter l&apos;application ?</translation>
     </message>
     <message>
-        <location filename="../../ui/askpopup.ui" line="124"/>
+        <location filename="../../ui/askpopup.ui" line="127"/>
         <source>The update will be stopped and the application will not be started</source>
         <translation>La mise à jour sera interrompue et l&apos;application ne démarrera pas</translation>
     </message>
     <message>
-        <location filename="../../ui/askpopup.ui" line="184"/>
+        <location filename="../../ui/askpopup.ui" line="187"/>
         <source>Yes</source>
         <translation>Oui</translation>
     </message>
     <message>
-        <location filename="../../ui/askpopup.ui" line="219"/>
+        <location filename="../../ui/askpopup.ui" line="222"/>
         <source>No</source>
         <translation>Non</translation>
     </message>
 </context>
 <context>
-    <name>Dialog</name>
+    <name>AuthenticationDialogUI</name>
     <message>
-        <location filename="../../ui/authenticationdialog.ui" line="20"/>
+        <location filename="../../ui/authenticationdialog.ui" line="38"/>
         <source>Credentials are required by the remote host</source>
         <translation>L&apos;hôte distant a besoin d&apos;un identifiant et d&apos;un mot de passe</translation>
     </message>
     <message>
-        <location filename="../../ui/authenticationdialog.ui" line="30"/>
+        <location filename="../../ui/authenticationdialog.ui" line="51"/>
         <source>Login</source>
         <translation>Identifiant</translation>
     </message>
     <message>
-        <location filename="../../ui/authenticationdialog.ui" line="40"/>
+        <location filename="../../ui/authenticationdialog.ui" line="61"/>
         <source>Password</source>
         <translation>Mot de passe</translation>
     </message>
     <message>
-        <location filename="../../ui/authenticationdialog.ui" line="64"/>
+        <location filename="../../ui/authenticationdialog.ui" line="102"/>
         <source>Site</source>
         <translation>Site</translation>
     </message>
@@ -55,53 +55,37 @@
 <context>
     <name>DownloadManager</name>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="424"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="367"/>
         <source>%1 %2</source>
         <extracomment>This string refers to download speed as &quot;12 ko/s&quot;.</extracomment>
         <translation>%1 %2</translation>
     </message>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="141"/>
-        <source>HTTP authentification required</source>
-        <translation>Authentification HTTP requise</translation>
-    </message>
-    <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="143"/>
-        <source>%1 on %2</source>
-        <comment>The realm on host</comment>
-        <translation>%1 sur %2</translation>
-    </message>
-    <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="181"/>
-        <source>Proxy authentication required</source>
-        <translation>Authentification proxy requise</translation>
-    </message>
-    <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="183"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="120"/>
         <source>%1</source>
         <comment>The proxy domain</comment>
         <translation>%1</translation>
     </message>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="412"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="355"/>
         <source>B/s</source>
         <extracomment>This string refers to bytes per second.</extracomment>
         <translation>o/s</translation>
     </message>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="416"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="359"/>
         <source>kB/s</source>
         <extracomment>This string refers to kilobytes per second.</extracomment>
         <translation>ko/s</translation>
     </message>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="420"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="363"/>
         <source>MB/s</source>
         <extracomment>This string refers to megabytes per second.</extracomment>
         <translation>Mo/s</translation>
     </message>
     <message numerus="yes">
-        <location filename="../../src/network/downloadmanager.cpp" line="435"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="378"/>
         <source>%n hour(s)</source>
         <comment>the estimated heures</comment>
         <translation>
@@ -110,7 +94,7 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location filename="../../src/network/downloadmanager.cpp" line="436"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="379"/>
         <source>%n minute(s)</source>
         <comment>the estimated minutes</comment>
         <translation>
@@ -119,7 +103,7 @@
         </translation>
     </message>
     <message numerus="yes">
-        <location filename="../../src/network/downloadmanager.cpp" line="437"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="380"/>
         <source>%n second(s)</source>
         <comment>the estimated seconds</comment>
         <translation>
@@ -128,43 +112,43 @@
         </translation>
     </message>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="445"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="388"/>
         <source>%1</source>
         <comment>seconds only</comment>
         <translation>%1</translation>
     </message>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="449"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="392"/>
         <source>%1</source>
         <comment>minutes only</comment>
         <translation>%1</translation>
     </message>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="451"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="394"/>
         <source>%1 %2</source>
         <comment>minutes and seconds only</comment>
         <translation>%1 %2</translation>
     </message>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="457"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="400"/>
         <source>%1</source>
         <comment>hours only</comment>
         <translation>%1</translation>
     </message>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="459"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="402"/>
         <source>%1 %2</source>
         <comment>hours and seconds only</comment>
         <translation>%1 %2</translation>
     </message>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="463"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="406"/>
         <source>%1 %2</source>
         <comment>hours and minutes only</comment>
         <translation>%1 %2</translation>
     </message>
     <message>
-        <location filename="../../src/network/downloadmanager.cpp" line="465"/>
+        <location filename="../../src/network/downloadmanager.cpp" line="408"/>
         <source>%1 %2 %3</source>
         <comment>hours minutes seconds</comment>
         <translation>%1 %2 %3</translation>
@@ -173,19 +157,19 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../../src/gui/mainwindow.cpp" line="158"/>
+        <location filename="../../src/gui/mainwindow.cpp" line="156"/>
         <source>Estimated time: %1</source>
         <extracomment>This string refers to a remaining time.</extracomment>
         <translation>Temps estimé : %1</translation>
     </message>
     <message>
-        <location filename="../../src/gui/mainwindow.cpp" line="165"/>
+        <location filename="../../src/gui/mainwindow.cpp" line="163"/>
         <source>Server %1</source>
         <extracomment>This string refers to a web URL.</extracomment>
         <translation>Serveur %1</translation>
     </message>
     <message>
-        <location filename="../../src/gui/mainwindow.cpp" line="171"/>
+        <location filename="../../src/gui/mainwindow.cpp" line="169"/>
         <source>Downloading %1</source>
         <extracomment>This string refers to a downloaded file.</extracomment>
         <translation>Téléchargement de %1</translation>
